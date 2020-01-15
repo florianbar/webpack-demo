9f919f2bff5b84e4a39134748228e002a7455eef
Flo <Florianb@payspace.com>
2020-01-15 15:17:59 +0200

    Added import for image

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
 import './style.css';
+import MyImage from './2020.jpg';
 
 function component() {
   const element = document.createElement('div');
+
+  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // Add the image to out existing div.
+  const myImage = new Image();
+  myImage.src = MyImage;
+  element.appendChild(myImage);
+
   return element;
 }
   
